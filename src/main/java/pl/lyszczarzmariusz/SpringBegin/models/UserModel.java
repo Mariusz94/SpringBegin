25507cef14f1a5797b2171becc7d669b079cac07
Mariusz Lyszczarz <lyszczarz.mariusz@gmail.com>
2018-05-10 22:54:09 +0200

    sample

--- src/main/java/pl/lyszczarzmariusz/SpringBegin/models/UserModel.java
+++ src/main/java/pl/lyszczarzmariusz/SpringBegin/models/UserModel.java
@@ -1,4 +1,22 @@
 package pl.lyszczarzmariusz.SpringBegin.models;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "user")
+@Data
+@NoArgsConstructor
 public class UserModel {
+    @Id
+    @GeneratedValue
+    private int id;
+    private String login;
+    private String password;
+    private int age;
 }
